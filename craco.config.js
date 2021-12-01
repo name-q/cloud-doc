29d757026e9c30f5fbfe9a4a4f82e3ddbf99af1e
钱雨 <1620206666@qq.com>
2021-12-01 15:15:47 +0800

    删除不必要文件  引入antd 主题  目录结构

---
+++ craco.config.js
@@ -0,0 +1,17 @@
+const CracoLessPlugin = require('craco-less');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { '@primary-color': '#F56614' },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
--- public/main.js
+++ public/main.js
@@ -14,10 +14,10 @@ const createWindow = () => {
     }
   });
   // 12版本后中不能直接require(electron).remote
-  // npm i -D @electron/remote
+  // yarn add -D @electron/remote
   require('@electron/remote/main').initialize()
   require('@electron/remote/main').enable(mainWindow.webContents)
-  // isDev 判断是开发模式还是产品模式，开发模式读取本地服务器，非开发模式读取 React 生成文件的 index.html 文件。
+  // isDev 判断是开发模式还是产品模式
   mainWindow.loadURL(isDev ? 'http://localhost:1234' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => mainWindow = null);
 };
