7ecea596311fa9907efe646a12bf6695dda90de2
钱雨 <1620206666@qq.com>
2021-12-10 17:44:53 +0800

    user info schema

---
+++ api/app/model/userinfo.js
@@ -0,0 +1,47 @@
+module.exports = app => {
+    const mongoose = app.mongoose;
+    const Schema = mongoose.Schema;
+
+    //(挂载名，实例化，集合名)
+    return mongoose.model(
+        'Userinfo',
+        new Schema({
+            // 邮箱
+            mail: {
+                type: String,
+                required: true,
+                unique: true,
+                trim: true,
+                maxlength: 50,
+                match: /^\w+@[a-zA-Z0-9]{2,10}(?:\.[a-z]{2,4}){1,3}$/
+            },
+            // 密码
+            password: {
+                type: String,
+                required: true,
+                trim: true,
+                maxlength: 20,
+                match: /^[0-9A-Za-z]+$/
+            },
+            // 昵称
+            nick: {
+                type: String,
+                required: true,
+                trim: true,
+                maxlength: 10,
+            },
+            // 最后登入时间
+            lastTime: {
+                type: Number,
+                default: 0,
+                validate: (d) => { return d >= 0 }
+            },
+            // 注册时间
+            registerTime: {
+                type: Number,
+                default: new Date().getTime(),
+            }
+        })
+        , 'user_info'
+    );
+}
