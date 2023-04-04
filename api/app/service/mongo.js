fdafa7a17fc3adec28057c22ec5e052df2eb4ce7
name-q <1620206666@qq.com>
2023-04-04 10:36:00 +0800

    筛选数据库返回值

--- api/app/service/mongo.js
+++ api/app/service/mongo.js
@@ -41,17 +41,15 @@ class MongoService extends Service {
    * @param {*} pageNum 当前页
    * @param {*} obj 查询条件
    * @param {?} sort 排序条件 -1降序 1正序
+   * @param {?} select 保留字段 不传默认全部返回 传则以空格区分的字符串
    * @returns
    */
-  async pagination(Schema, pageSize, pageNum, obj, sort) {
-    return sort
-      ? this.ctx.model[Schema].find(obj)
-          .limit(pageSize)
-          .skip((pageNum - 1) * pageSize)
-          .sort(sort)
-      : this.ctx.model[Schema].find(obj)
-          .limit(pageSize)
-          .skip((pageNum - 1) * pageSize);
+  async pagination(Schema, pageSize, pageNum, obj, sort, select) {
+    return this.ctx.model[Schema].find(obj)
+      .limit(pageSize)
+      .skip((pageNum - 1) * pageSize)
+      .sort(sort)
+      .select(select);
   }
 }
 module.exports = MongoService;
