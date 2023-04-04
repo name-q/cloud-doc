e112787932e091feebd4ab482b23981ef1d5e4e0
name-q <1620206666@qq.com>
2023-04-04 17:06:02 +0800

    css-in-js主题色

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import 'default-passive-events'
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import "default-passive-events";
 
-import './index.less';
-import App from './App';
+import { ConfigProvider } from "antd";
+
+import "./index.less";
+import App from "./App";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-  ,
-  document.getElementById('root')
+  <ConfigProvider
+    theme={{
+      token: {
+        colorPrimary: "#F56614",
+        colorFillContentHover: "#F56614",
+        colorTextHeading: "#F56614",
+        colorLink: "#F56614",
+        colorLinkHover: "rgb(241,144,79)",
+      },
+    }}
+  >
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ConfigProvider>,
+  document.getElementById("root")
 );
